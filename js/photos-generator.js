9f20e4080f686656a0f5272a8c4d4b2546d76434
romkafomin <fomin.romka@yandex.ru>
2024-02-02 17:12:13 +0300

    5.1 разбил код на модули

---
+++ js/photos-generator.js
@@ -0,0 +1,30 @@
+import {getRandomInteger, generatePhotoId, generatePhotoUrl, generateCommentId} from './util.js';
+import {COMMENTS_ARRAY_LENGTH, NAMES_LIST, DESCRIPTION_LIST} from './data'
+
+//функция для генерации массива комментариев
+function getComments () {
+  return {
+    id: generateCommentId(),
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: COMMENTS_LIST[getRandomInteger(0, COMMENTS_LIST.length - 1)],
+    name: NAMES_LIST[getRandomInteger(0, NAMES_LIST.length - 1)],
+  }
+};
+
+//функция для создаания фотографии опубликованной юзером
+function makeUserPhoto () {
+  return {
+    id: generatePhotoId(),
+    url: `photos/${generatePhotoUrl()}.jpg`,
+    description: DESCRIPTION_LIST[getRandomInteger(0, DESCRIPTION_LIST.length -1)],
+    likes: getRandomInteger(15,200),
+    comments: Array.from({length: getRandomInteger(COMMENTS_ARRAY_LENGTH.min,COMMENTS_ARRAY_LENGTH.max)}, getComments)
+  }
+};
+
+const generateArrayPhoto = Array.from({length: PHOTOS_COUNT}, makeUserPhoto);
+
+export {makeUserPhoto};
+
+
+
---
+++ js/util.js
@@ -0,0 +1,33 @@
+import {PHOTOS_COUNT} from './data.js';
+
+//функция получения рандомного числа в диапзоне от a до b
+function getRandomInteger (a, b) {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//функция для получения уникального id в диапазоне от min до max
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+const generatePhotoId = createRandomIdFromRangeGenerator(1, PHOTOS_COUNT);
+const generatePhotoUrl = createRandomIdFromRangeGenerator(1, PHOTOS_COUNT);
+const generateCommentId = createRandomIdFromRangeGenerator(1, 1000);
+
+export {getRandomInteger, createRandomIdFromRangeGenerator, generatePhotoId, generatePhotoUrl, generateCommentId};
