c28d43c8110579acca5a93cb8b0f5b722c7c2964
romkafomin <fomin.romka@yandex.ru>
2024-02-29 18:59:01 +0300

    8.1 fixes

--- js/big-pictures.js
+++ js/big-pictures.js
@@ -2,21 +2,23 @@ import { generateArrayPhoto } from './main.js';
 import {getComments} from './photos-generator.js';
 
 const bigPictureBlock = document.querySelector('.big-picture');
-const socialCommentCount = document.querySelector('.social__comment-count');
-const commentsLoader = document.querySelector('.comments-loader');
-const bodyElement = document.querySelector('.body');
-const closeButton = document.querySelector('.big-picture__cancel');
+const socialCommentCount = bigPictureBlock.querySelector('.social__comment-count');
+const commentsLoader = bigPictureBlock.querySelector('.comments-loader');
+const bodyElement = document.querySelector('body');
+const closeButton = bigPictureBlock.querySelector('.big-picture__cancel');
+const pictureBlock = document.querySelector('.pictures');
 
-function openFullSize (evt) {
-  evt.preventDefault();
+
+//удаляем/добавляем нужные классы для показа полного изображения, вешаем слушатель нажатия кнопки закрытия
+function openFullSize () {
   bigPictureBlock.classList.remove('hidden');
   socialCommentCount.classList.add('hidden');
   commentsLoader.classList.add('hidden');
   bodyElement.classList.add('modal-open');
-
   document.addEventListener('keydown', pushEscButton);
 }
 
+//функция закрытия полноразмерного изображения, удаление обработчика
 function closeFullSize () {
   bigPictureBlock.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
@@ -24,6 +26,7 @@ function closeFullSize () {
   document.removeEventListener('keydown', pushEscButton);
 }
 
+//функция закрытия по нажатию на клавишу esc
 function pushEscButton (evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
@@ -31,14 +34,15 @@ function pushEscButton (evt) {
   }
 }
 
-bigPictureBlock.addEventListener('click', () => {
-  openFullSize();
-});
-
 closeButton.addEventListener('click', () => {
   closeFullSize();
 });
 
+pictureBlock.addEventListener('click', () => {
+  openFullSize();
+});
+
+//создаем полноразмерную фотографию
 function renderBigPicture ({id, url,likes,comments,description}) {
   document.querySelector('.big-picture__img').src = url;
   document.querySelector('.big-picture__img').id = id;
@@ -48,6 +52,7 @@ function renderBigPicture ({id, url,likes,comments,description}) {
   getComments(comments);
 }
 
+//открываем полноразмерную фотографию через поиск по id
 function openFullSizePicture (photoId) {
   const selectedPhoto = generateArrayPhoto.find((photo) => photo.id === photoId);
   if (selectedPhoto) {
--- js/miniatures.js
+++ js/miniatures.js
@@ -1,8 +1,9 @@
-import {openFullSizePicture} from './big-pictures';
+import {openFullSizePicture} from './big-pictures.js';
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictureBlock = document.querySelector('.pictures');
 
+//создаем миниатюру, вешаем обработчик для открытия полноразмерной фотографии
 function createPicture ({url,likes,comments,description,id}) {
   const pictureElement = pictureTemplate.cloneNode(true);
   const imgElement = pictureElement.querySelector('.picture__img');
@@ -11,10 +12,13 @@ function createPicture ({url,likes,comments,description,id}) {
   imgElement.dataset.id = id;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  pictureElement.addEventListener('click' , openFullSizePicture());
+  pictureBlock.addEventListener('click' , () => {
+    openFullSizePicture();
+  });
   return pictureElement;
 }
 
+//создаем массив миниатюр и добавляем их на страницу через шаблон
 function createAllPicture (pictures) {
   const pictureFragment = document.createDocumentFragment();
   pictures.forEach ((picture) => {
